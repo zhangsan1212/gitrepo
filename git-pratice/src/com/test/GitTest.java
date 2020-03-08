3ef7b02a9453fa6b8e992a1661d7ee6d3617e600
zhangsan1212 <924098345@qq.com>
2020-03-08 21:16:04 +0800

    提交到本地仓库

---
+++ git-pratice/src/com/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git----------");
+        System.out.println("git----------");
+        System.out.println("git----------");
+        System.out.println("git----------");
+        System.out.println("git----------");
+        System.out.println("git----------");
+        System.out.println("git----------");
+    }
+}
