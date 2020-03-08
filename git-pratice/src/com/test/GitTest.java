fa000fb21db563e34ebf3f4fb4dad4b66ae0f77d
zhangsan1212 <924098345@qq.com>
2020-03-08 21:27:03 +0800

    提交到本地仓库git-practice

--- git-pratice/src/com/test/GitTest.java
+++ git-pratice/src/com/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("git----------");
         System.out.println("git----------");
         System.out.println("git----------");
+        System.out.println("git--practice");
     }
 }
